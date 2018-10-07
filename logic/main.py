e3fc0ce9f611df816930d285c7a665b6573a67be
rcy <rcycyzh@163.com>
2018-10-07 23:41:55 +0800

    add resource and logic, remove some files, layout the respository

---
+++ logic/main.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+def main():
+    # initialize the game
+
+    # start the loop
+
+    # report the final result
+
+    return
+
+
+if __name__ == '__main__':
+    main()
---
+++ resource/readme.md
@@ -0,0 +1,6 @@
+此处用来存放可能用到的供阅读之用的资源文件。
+
+# 文件列表
+
+- slope.jpg：rule.md配图
+- vision.jpg：rule.md配图
